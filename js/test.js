49a25a08ac51da100a8ad27f2cae4c020eaf6a27
Adam Auckland <adamauckland@gail.com>
2013-07-14 17:39:13 +0100

    added lists

--- js/test.js
+++ js/test.js
@@ -1,17 +1,82 @@
 function NearlyWygEditor(styleSelector, textArea, copyArea, buttonBlock) {
-    var styleBlock = new StyleBlock($(styleSelector)),
-        elementBlock = new EditorBlock($(textArea)),
-        $copyArea = $(copyArea);
-        $buttonBlock = $(buttonBlock),
+    var styleBlock,
+        elementBlock, 
+        $styleSelector,
+        $textArea,
+        $copyArea,
+        $buttonBlock,
         $latestElement = null,
         cancelButtonEvent = false;
     
-    elementBlock.styleBlock = styleBlock;
-    styleBlock.elementBlock = elementBlock;
     
-    $buttonBlock.hide();
+    function generateHtmlForContent(elementType, content) {
+        var $rootElement,
+            $itemElement;
+        
+        $rootElement = $('<'.concat(elementType).concat('/>'));
+          
+        switch(elementType) {
+            case 'ol':
+            case 'ul':
+                // split content into carriage return separated items, then
+                // add as list items
+                var splitItem = content.split('\n');
+                
+                for(var i=0; i<splitItem.length; i++) {
+                    $itemElement = $('<li />').text(splitItem[i]);
+                    $rootElement.append($itemElement);
+                }
+                break;
+            
+            default:
+                buildHtml = $rootElement.text(content);
+                break;
+        }
+
+        return $rootElement;
+    }
     
-    attachButtonClickHandlers();
+    
+    function generateContentForHtml($element) {
+        var elementType = $element.prop('tagName').toLowerCase(),
+            content,
+            buildItems;
+        
+        switch (elementType) {
+            case 'ol':
+            case 'ul':
+                buildItems = [];
+                $element.find('li').each(function(index, element) {
+                    var $liElement = $(element);
+                    buildItems.push($liElement.text());
+                });
+                
+                content = buildItems.join('\n');
+                break;
+            default:
+                content = $element.text();
+        };
+        
+        return content;
+    }
+    
+    
+    function initialise() {
+        $styleSelector = $(styleSelector);
+        $textArea = $(textArea);
+        $copyArea = $(copyArea);
+        $buttonBlock = $(buttonBlock);
+        
+        styleBlock = new StyleBlock($styleSelector);
+        elementBlock = new EditorBlock($textArea);
+        
+        elementBlock.styleBlock = styleBlock;
+        styleBlock.elementBlock = elementBlock;
+        
+        $buttonBlock.hide();
+        
+        attachButtonClickHandlers();        
+    }
     
     
     function attachButtonClickHandlers() {
@@ -52,6 +117,8 @@ function NearlyWygEditor(styleSelector, textArea, copyArea, buttonBlock) {
         
         
         function startEditingBlock(blockElement, newElementType) {
+            var elementText;
+            
             if (self.newElement !== true) {
                 if (self.newElement.hasClass('editing')) {                
                     self.newElement.removeClass('editing');
@@ -70,8 +137,7 @@ function NearlyWygEditor(styleSelector, textArea, copyArea, buttonBlock) {
             self.newElementType = newElementType;
             self.newElement = blockElement;
             
-            var elementText = blockElement.html();
-            elementText = elementText.replace(/<br>/g, '\n');
+            elementText = generateContentForHtml(blockElement);
             
             self.element.val(elementText);        
             self.element.focus();
@@ -96,14 +162,50 @@ function NearlyWygEditor(styleSelector, textArea, copyArea, buttonBlock) {
         }
         
         
-        function blurFromElement() {
+        this.updateCopyNewItem = function() {
             var newElementType,
                 newVal,
                 newHtml,
-                stripText,
+                
                 $newElement,
                 $newReplaceHtml,
                 $newReplaceElement;
+            
+            newElementType = self.styleBlock.getElementType();
+            $newElement = generateHtmlForContent(newElementType, self.element.val());
+            
+            attachBlockClickHandlers($newElement, newElementType);
+            
+            $copyArea.append($newElement);
+            
+            $latestElement = $newElement;
+        }
+        
+        
+        this.updateCopyExistingItem = function() {
+            var newElementType,
+                newVal,
+                newHtml,
+                $newElement,
+                $newReplaceHtml,
+                $newReplaceElement;
+                
+            newElementType = self.styleBlock.getElementType();            
+            
+            self.newElement.removeClass('editing');
+            
+            $newReplaceElement = generateHtmlForContent(newElementType, self.element.val());
+            
+            attachBlockClickHandlers($newReplaceElement, newElementType);
+            
+            self.newElement.replaceWith($newReplaceElement);
+            
+            $latestElement = $newReplaceElement;
+        }
+        
+        
+        function blurFromElement() {
+            var stripText;
             
             if (cancelButtonEvent === true) {
                 cancelButtonEvent = false;
@@ -124,45 +226,20 @@ function NearlyWygEditor(styleSelector, textArea, copyArea, buttonBlock) {
                 return; 
             }
             
-            /// new element, create another item
-            if (self.newElement === true) {
-                stripText = self.element.val().replace(/ /g,'');
-                
-                if (stripText !== '') {                
-                    newElementType = self.styleBlock.getElementType(); //self.styleBlock.element.find('*[data-selected=true]').attr('data-element');
-                    newVal = self.element.val().replace(/\n/g, '<br/>');
-                    newHtml = '<'.concat(newElementType).concat('>').concat(newVal).concat('</').concat(newElementType).concat('>');
-                    $newElement = $(newHtml);
-                    
-                    attachBlockClickHandlers($newElement, newElementType);
-                    
-                    $copyArea.append($newElement);
-                    
-                    $latestElement = $newElement;
+            stripText = self.element.val().replace(/ /g,'');
+            if (stripText !== '') {
+                if (self.newElement === true) {
+                    self.updateCopyNewItem();
+                } else {
+                    self.updateCopyExistingItem();
                 }
             } else {
-                stripText = self.element.val().replace(/ /g,'');
-                
-                if (stripText !== '') {                        
-                    newElementType = self.styleBlock.getElementType();            
-                    newVal = self.element.val().replace(/\n/g, '<br/>');          
-                    newReplaceHtml = '<'.concat(newElementType).concat('>').concat(newVal).concat('</').concat(newElementType).concat('>');
-                    
-                    self.newElement.removeClass('editing');
-                    
-                    $newReplaceElement = $(newReplaceHtml);
-                    
-                    attachBlockClickHandlers($newReplaceElement, newElementType);
-                    
-                    self.newElement.replaceWith($newReplaceElement);
-                    
-                    $latestElement = $newReplaceElement;
-                } else {
+                // if we're editing an existing item and the user has cleared the item,
+                // delete it
+                if (self.newElement !== true) {
                     self.newElement.remove();
-                    
                     $latestElement = null;
                 }
-                console.log('new html' + newReplaceHtml);
             }
             
             cancelSorting();
@@ -198,7 +275,6 @@ function NearlyWygEditor(styleSelector, textArea, copyArea, buttonBlock) {
             self.element.find('li').each(function(index, element) {
                 var $element = $(element);
                 if ($element.attr('data-selected') === 'true') {
-                    console.log('returning ' + $element.attr('data-element'))
                     elementTypeReturn = $element.attr('data-element');
                 }
             });
@@ -251,6 +327,11 @@ function NearlyWygEditor(styleSelector, textArea, copyArea, buttonBlock) {
                 collapseMenu();
                 showStyles = false;
                 
+                // change existing content
+                if (self.elementBlock.newElement !== true) {
+                    //self.elementBlock.updateCopyExistingItem();
+                }
+                
                 self.elementBlock.element.focus();
             }
         }
@@ -279,4 +360,6 @@ function NearlyWygEditor(styleSelector, textArea, copyArea, buttonBlock) {
         bindClickHandlers();
         collapseMenu();    
     }
+    
+    initialise();
 }
